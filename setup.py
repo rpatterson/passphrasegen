1302e84fd8796bb75736a90b80f4a94bf8bf0031
Ross Patterson <me@rpatterson.net>
2013-08-11 17:43:57 -0700

    Add dist classifiers

--- setup.py
+++ setup.py
@@ -18,7 +18,16 @@ setup(name='passphrasegen',
       version=version,
       description=passphrasegen.__doc__.strip(),
       long_description=long_description,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Topic :: Security',
+          'Topic :: Utilities',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='password generator passphrase entropy',
       author='Ross Patterson',
       author_email='me@rpatterson.net',
